0d1c7c0bc17d6ee2aa6a6152591b6a7b977e3d07
dario-nihil <dario.nihil@gmail.com>
2023-12-19 21:29:15 +0100

    implemented TaxIncludedPriceJob structure to represent the tax and price plus tax

--- main.go
+++ main.go
@@ -1,22 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"example.com/price_calculator/prices"
+)
 
 func main() {
-	prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 1, 0.15}
 
-	result := make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
-		taxIncludedPrices := make([]float64, len(prices))
-
-		for idx, price := range prices {
-			taxIncludedPrices[idx] = price * (1 + taxRate)
-		}
-
-		result[taxRate] = taxIncludedPrices
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
-
-	fmt.Println(result)
 }
