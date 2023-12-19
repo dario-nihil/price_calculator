4dca7f4ed3bea0d3ff354c9495e023daf853d74b
dario-nihil <dario.nihil@gmail.com>
2023-12-19 21:04:18 +0100

    created the skeleton of the application logic

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	prices := []float64{10, 20, 30}
+	taxRates := []float64{0, 0.07, 1, 0.15}
+
+	result := make(map[float64][]float64)
+
+	for _, taxRate := range taxRates {
+		taxIncludedPrices := make([]float64, len(prices))
+
+		for idx, price := range prices {
+			taxIncludedPrices[idx] = price * (1 + taxRate)
+		}
+
+		result[taxRate] = taxIncludedPrices
+	}
+
+	fmt.Println(result)
 }
